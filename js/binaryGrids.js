70ddaefeef04e42a53e4b1273bf1bcdba3cee64e
J. Cohen <hostelmarketing@gmail.com>
2014-03-21 13:38:57 -0700

    Better way

--- js/binaryGrids.js
+++ js/binaryGrids.js
@@ -1,36 +1,41 @@
-
+// The array of all 512 grids
 var grids = [];
 
-// outer loop 512 times
+// Holds array of every possible row, indexed rowsArr[0] to rowsArr[7]
+rowsArr = createRowsArr();
+
+// Loop 512 times, once for each possible grid
 for (var i = 0; i < 512; i++) {
+    var curIndexToString = i.toString();
+    //var curGrid = [];
+    curIndexSplit = curIndexToString.split('');
+    console.log(curIndexSplit);
 
-    var curGrid = [];
-
-    // inner loop 3 times
+    // Inner loop 3 times
     for (var i = 0; i < 3; i++) {
 
         // create a row three times, unshifting the inner array
-        x = createRow();
+        x = createRows();
         
     }
     
 }
 
-function createRow() {
-    var row = [];
-    for (var i = 0; i <= 7; i++) {
-        x = convertToBinary(i);
+// Create an array of all possible rows 000-111
+function createRowsArr() {
+    var rows = [],
+        row;
 
-        // Unshift the row
-        row.unshift(x);
-        console.log(curRow);
+    for (var i = 0; i <= 7; i++) {
+        row = convertToBinary(i);
+        rows.push(row);
     }
+    return rows;
 }
 
 // Convert to 3-digit binary
-function convertToBinary(num) {
-    var num = i.toString(2);
+function convertToBinary(digit) {
+    var num = digit.toString(2);
     while (num.length < 3) num = "0" + num;
-    console.log(num);
     return num;
 }
