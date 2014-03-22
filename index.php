05d07b521fea13d7f70dabb34fbeb5a0d02fc9c7
J. Cohen <hostelmarketing@gmail.com>
2014-03-22 13:15:56 -0700

    Add CSS and button

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title>3x3 Binary Matrices / Grids for Memorization</title>
+    <title>3x3 Binary Number Matrices / Grids for Memorization</title>
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure-min.css">
 <style>
 .grid {
     float:left;
     font-family: "Courier New", monospace;
-    margin: 19px;
-    padding: 9px;
+    font-size: 1.2em;
+    margin: 9px;
+    letter-spacing: 0.5em;
+    padding: 3px;
+}
+body {
+    margin: 0 40px 40px 40px;
+}
+.button-xsmall {
+    font-size:70%;
+}
+.button-small {
+    font-size:85%;
+}
+.answer-input{
+    margin-right:7px;
+}
+.button-success {
+    background:rgb(28,184,65);
 }
 </style>
 </head>
 <body>
-<h1>3x3 Binaries</h1>
-<p>All 512 possibly 3x3 binary grids, randomized.</p>
-<div id="gridOutput"></div>
+<div class="pure-g-r">
+    <div class="pure-u-1-1">
+        <h1>3x3 Binaries</h1>
+        <p>All 512 possible 3x3 binary grids, randomized. Also check out our other <a href="/training">memory training tools</a>. <button id="randomizeGrids" class="button-warning pure-button">Randomize Grids</button></p>
+        <div id="gridOutput">
+            <div id="loader"><img src="images/loader.gif"></div>
+        </div>
+    </div>
+</div>
+
 
 <script id="gridTemplate" type="x-handlebars-template">
 {{#each grids}}
@@ -21,7 +47,7 @@
     <div class="rowOne">{{ rowOne }}</div>
     <div class="rowTwo">{{ rowTwo }}</div>
     <div class="rowThree">{{ rowThree }}</div>
-    </div>
+</div>
 {{/each}}
 </script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
--- js/binaryGrids.js
+++ js/binaryGrids.js
@@ -1,85 +1,91 @@
-var gridsObject = {};
-var grids = [];
+$(document).ready(function() {
+    var gridsObject = {};
+    var grids = [];
 
-// Holds array of every possible row, indexed rowsArr[0] to rowsArr[7]
-var rowsArr = createRowsArr();
+    // Holds array of every possible row, indexed rowsArr[0] to rowsArr[7]
+    var rowsArr = createRowsArr();
 
-// TODO: Clean this up
-var curIndexToString,
-    curIndexSplit,
-    curRow,
-    curGrid,
-    y,
-    outerIndex,
-    stringifiedOuterIndex,
-    outerIndex,
-    curGridIndexSplit;
+    // TODO: Clean this up
+    var curIndexToString,
+        curIndexSplit,
+        curRow,
+        curGrid,
+        y,
+        outerIndex,
+        stringifiedOuterIndex,
+        outerIndex,
+        curGridIndexSplit;
 
-// Loop 512 times, once for each possible grid
-for (var i = 0; i < 512; i++) {
-    // Clear the current stuff
-    curGrid = {};
+    // Loop 512 times, once for each possible grid
+    for (var i = 0; i < 512; i++) {
+        // Clear the current stuff
+        curGrid = {};
 
-    // Convert i to zero-padded, nine-digit binary
-    curBinary = padNumber(convertToBinary(i), 9);
-    curBinaryInArray = curBinary.match(/.{1,3}/g); // 3-item array of the current binary
-    //console.log(curBinaryInArray);
+        // Convert i to zero-padded, nine-digit binary
+        curBinary = padNumber(convertToBinary(i), 9);
+        curBinaryInArray = curBinary.match(/.{1,3}/g); // 3-item array of the current binary
+        //console.log(curBinaryInArray);
 
-    // Inner loop three times, pop three each time
-    for (var j = 0; j < 3; j++) {
-        rowName = nameTheRow(j);
-        curGrid[rowName] = curBinaryInArray[j];
+        // Inner loop three times, pop three each time
+        for (var j = 0; j < 3; j++) {
+            rowName = nameTheRow(j);
+            curGrid[rowName] = curBinaryInArray[j];
+        }
+        grids.push(curGrid);
     }
-    grids.push(curGrid);
-}
-grids = _.shuffle(grids);
-gridsObject.grids = grids;
+    grids = _.shuffle(grids);
+    gridsObject.grids = grids;
 
-function nameTheRow(row) {
-    var rowName;
-    switch(row) {
-        case 0:
-            rowName = "rowOne";
-            break;
-        case 1:
-            rowName = "rowTwo";
-            break;
-        case 2:
-            rowName = "rowThree";
-            break;
+    function nameTheRow(row) {
+        var rowName;
+        switch(row) {
+            case 0:
+                rowName = "rowOne";
+                break;
+            case 1:
+                rowName = "rowTwo";
+                break;
+            case 2:
+                rowName = "rowThree";
+                break;
+        }
+        return rowName;
     }
-    return rowName;
-}
-function compileTemplate() {
-    var source = $("#gridTemplate").html();
-    var template = Handlebars.compile(source);
-    var context = gridsObject;
-    var html = template(context);
-    $("#gridOutput").append(html);
-}
-// Create an array of all possible rows 000-111
-function createRowsArr() {
-    var rows = [],
-        row;
-
-    for (var i = 0; i <= 7; i++) {
-        row = convertToBinary(i);
-        rows.push(row);
+    function compileTemplate() {
+        var source = $("#gridTemplate").html();
+        var template = Handlebars.compile(source);
+        var context = gridsObject;
+        var html = template(context);
+        $("#loader").hide();
+        $("#gridOutput").append(html);
     }
-    return rows;
-}
+    // Create an array of all possible rows 000-111
+    function createRowsArr() {
+        var rows = [],
+            row;
 
-// Convert to 3-digit binary
-function convertToBinary(num) {
-    var output = num.toString(2);
-    return output;
-}
+        for (var i = 0; i <= 7; i++) {
+            row = convertToBinary(i);
+            rows.push(row);
+        }
+        return rows;
+    }
 
-// Pad a number with zeros to length of 3 and return as string
-function padNumber(num, padLen) {
-    var theNum = num.toString();
-    while (theNum.length < padLen) theNum = "0" + theNum;
-    return theNum;
-}
+    // Convert to 3-digit binary
+    function convertToBinary(num) {
+        var output = num.toString(2);
+        return output;
+    }
 
-compileTemplate();
+    // Pad a number with zeros to length of 3 and return as string
+    function padNumber(num, padLen) {
+        var theNum = num.toString();
+        while (theNum.length < padLen) theNum = "0" + theNum;
+        return theNum;
+    }
+    compileTemplate();
+    $("#randomizeGrids").on("click", function(e) {
+        e.preventDefault();
+        location.reload(true);
+    });
+});
